fdf19c7d66fee75872cb2bcaf1804df7e039179f
Dan Nicholson <nicholson@endlessm.com>
2018-06-11 07:04:22 -0500

    obs-api: Drop cron jobs
    The cron.d jobs shipped with obs-api are no longer needed. OBS now
    manages its own timed jobs for these. Furthermore, the paths were wrong
    and it was just sending a failed cron email every minute.
    
    https://phabricator.endlessm.com/T14726

--- debian/obs-api.cron.d
+++
@@ -1,3 +0,0 @@
-* * * * *       www-data    cd /srv/www/obs/api/ && RAILS_ENV=production rake -s jobs:workerstatus > /dev/null
-# Disabled: it is apparently used for updating non existing obs bugzilla. SIN: #55
-#1 * * * *       www-data    cd /srv/www/obs/api/ && RAILS_ENV=production rake -s jobs:updateissues > /dev/null
