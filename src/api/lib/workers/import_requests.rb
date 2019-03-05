75adae441cda3dcae30a5b5a9fde9333d01ea5a5
Dan Nicholson <nicholson@endlessm.com>
2019-03-05 11:30:27 -0600

    [api] Log errors from ImportRequestsDelayedJob
    Send errors to the rails log instead of stdout and include an error
    message when the request can't be retrieved.
    
    https://phabricator.endlessm.com/T23473

--- src/api/lib/workers/import_requests.rb
+++ src/api/lib/workers/import_requests.rb
@@ -9,13 +9,15 @@ class ImportRequestsDelayedJob
     while lastrq > 0
       begin
         xml = Suse::Backend.get( "/request/#{lastrq}" ).body
-      rescue ActiveXML::Transport::Error
+      rescue ActiveXML::Transport::Error => err
+        Rails.logger.error "Request ##{lastrq} could not be retrieved:\n#{err}"
         lastrq -= 1
         next
       end
       r = BsRequest.new_from_xml xml
       unless r.save
-        puts "Request ##{lastrq}:", r.errors.full_messages.join("\n")
+        Rails.logger.error "Request ##{lastrq} could not be saved:\n%s" \
+                           % r.errors.full_messages.join("\n")
       end
       lastrq -= 1
     end
